54271ce3d8c922b3a43c63c0111c99c03ca2d23c
Kyle <phamky@colostate.edu>
2024-08-29 09:28:07 -0600

    Updated hamburger for meeting-times

---
+++ index.js
@@ -0,0 +1,7 @@
+// Toggle the visibility of the nav links on hamburger menu click
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
