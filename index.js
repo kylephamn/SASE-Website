25f4a1c7284212289c8f2c3fd1acb2588cd177f9
Kyle <97947120+kylephamn@users.noreply.github.com>
2024-12-05 22:13:20 +0000

    removed backend

--- index.js
+++
@@ -1,9 +0,0 @@
-// index.js
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
-
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('show');
-    });
-});
